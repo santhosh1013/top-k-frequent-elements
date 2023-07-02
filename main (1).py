5c00cf4c6f09a53f04c03937e880628445972343
santhosh1013 <98205205+santhosh1013@users.noreply.github.com>
2023-07-02 10:11:40 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,19 @@
+count={}
+k=2
+nums=[1,1,2,3,4,5]
+freq=[[] for i in range(len(nums)+1)]
+
+for n in nums:
+    count[n]=1+count.get(n,0)
+for n,c in count.items():
+    freq[c].append(n)
+print(freq,end="\n")    
+res=[]
+for i in range(len(freq)-1,0,-1):
+    for n in freq[i]:
+        res.append(n)
+        if len(res)==k:
+            print(res)
+            
+            
+    
